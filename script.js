aedabc9507e9f68453393799e7c06d99e8954b1a
Andrew Vreeland <andrewvreeland@gmail.com>
2023-01-20 22:21:00 -0500

    good stopping point, struggling with pulling the userName input data to be used in later functions, will consult with peers. tomorrows task will be adding more prompts with OR and AND statements

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+function welcome() {
+    document.write('Welcome to the Sunshine Page');
+}
+
+function userName() {
+    let userName = prompt('Hello, what is your name?');
+    if (userName === '')
+        alert("Hey Don't forget to put in your Username!")
+    if (userName === '')
+        console.log('User left their name blank');
+    else console.log('Their username is', userName);
+
+    while (userName === '') {
+        userName = prompt('You need to input a name to continue');
+        if (userName === '')
+            console.log('User left their name blank');
+        else console.log('Their username is', userName);
+    }
+}
+
+// function cuteTurtles() {
+//     let cuteTurtles = prompt('Would you rather see cute Turtles or cute Manatees?');
+//     console.log(userName, ' chose ', cuteTurtles)
+   
+// }
 
