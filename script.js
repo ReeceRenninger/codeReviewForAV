4f2c3f89140a3a0ff57729b896550177c52a51ff
Andrew Vreeland <andrewvreeland@gmail.com>
2023-01-21 11:36:14 -0500

    still stuck...

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function welcome() {
 function userName() {
     let userName = prompt('Hello, what is your name?');
     if (userName === '')
-        alert("Hey Don't forget to put in your Username!");
+        alert("Hey, Don't forget to put in your Username!");
     if (userName === '')
         console.log('User left their name blank');
     else console.log('Their username is', userName);
@@ -23,23 +23,28 @@ function userName() {
 // The while statement is not working
 function cuteAnimal() {
     let cuteAnimal = prompt('Would you rather see cute Turtles or cute Manatees?');
+    if (cuteAnimal === 'cute turtles' || cuteAnimal === 'turtles' || cuteAnimal === 'cute manatees' || cuteAnimal === 'manatees')
+    console.log('They chose ' + cuteAnimal + '!');
+    if (cuteAnimal !== 'cute turtles' || cuteAnimal !== 'turtles' || cuteAnimal !== 'cute manatees' || cuteAnimal !== 'manatees')
+        alert('Hey, I need you to choose between Manatees or Turtles!');
+    else (cuteAnimal === 'cute turtles' || cuteAnimal === 'turtles' || cuteAnimal ===  'cute manatees' || cuteAnimal === 'manatees');{ 
+        let userAmount = prompt('how many ' + cuteAnimal + ' would you like to see ' + '?')};
+        // checking to see if cute animal equals one of the above.
 
-     console.log('They chose ' + cuteAnimal + '!');
-     if (!(cuteAnimal === 'cute turtles' ||'turtles' || 'cute manatees' || 'manatees')); 
-      alert('Hey I need you to choose between Manatees or Turtles!');
+        while (cuteAnimal !== 'cute turtles' || cuteAnimal !== 'turtles' || cuteAnimal !== 'cute manatees' || cuteAnimal !== 'manatees') {
+            cuteAnimal = prompt('Hey, I need you to choose between cute Turtles or cute Manatees')
 
-      while (cuteAnimal !== 'cute turtles' ||'turtles' || 'cute manatees' || 'manatees'){
- cuteAnimal = prompt('Hey I need you to choose between cute Turtles or cute Manatees')
-      }
-     if (cuteAnimal === 'cute turtles' || 'turtles' || 'cute manatees' || 'manatees' ) {
-         let userAmount = prompt('how many ' + cuteAnimal + ' would you like to see ' + '?');
+            if (cuteAnimal === 'cute turtles' || cuteAnimal ===  'turtles' || cuteAnimal ===  'cute manatees' || cuteAnimal ===  'manatees') {
+                let userAmount = prompt('how many ' + cuteAnimal + ' would you like to see ' + '?');
+             }
+             else if (console.log('They chose ' + cuteAnimal + '!'));
         }
-
+    }
 
 
 // console.log('The user chose ' + userAmount + cuteAnimal);
 
-}
+
 
 
 
